a25efb07e87be293a5301a6c29f61fa4c6b76e36
clavijo99 <bclavijogomez@gmail.com>
2025-09-02 10:15:28 -0500

    Refactor splash.dart to improve splash screen layout and add navigation to login screen

--- Flutter/navegacion/lib/pages/splash.dart
+++ Flutter/navegacion/lib/pages/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navegacion/pages/login.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,6 +9,15 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+@override
+  void initState() {
+    Future.delayed( Duration( seconds: 3 ) , () {
+      Navigator.of(context).pushReplacement( MaterialPageRoute(builder: (_) => LoginScreen() ) );
+    } );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +32,22 @@ class _SplashScreenState extends State<SplashScreen> {
           ),
           Positioned.fill(
             child: Padding(
-              padding: EdgeInsets.only(bottom: 100),
+              padding: EdgeInsets.only(
+                bottom: 100,
+              ), // all() - todos los lados, only() - me permite eliger donde aplicar el padding, symmetry() - horizontal o vertical
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
-                children: [Text("Cargando" , style: TextStyle(color: Colors.white, fontSize: 30),), CircularProgressIndicator()],
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      "Cargando",
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  CircularProgressIndicator(),
+                ],
               ),
             ),
           ),
