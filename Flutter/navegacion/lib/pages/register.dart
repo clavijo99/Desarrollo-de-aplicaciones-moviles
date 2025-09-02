830bd0ec086c3eaf2e6b0de09f0ff033952a2f6e
clavijo99 <bclavijogomez@gmail.com>
2025-09-02 09:49:33 -0500

    Add initial layout, login, register, and splash screens

---
+++ Flutter/navegacion/lib/pages/register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisteScreen extends StatefulWidget {
+  const RegisteScreen({super.key});
+
+  @override
+  State<RegisteScreen> createState() => _RegisteScreenState();
+}
+
+class _RegisteScreenState extends State<RegisteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
