d20e841d961c471437e6572d502c6a1f043b0d20
clavijo99 <bclavijogomez@gmail.com>
2025-09-02 10:06:24 -0500

    Refactor main.dart to use SplashScreen and implement splash screen layout in splash.dart

--- Flutter/navegacion/lib/pages/splash.dart
+++ Flutter/navegacion/lib/pages/splash.dart
@@ -10,6 +10,27 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/imagenes/fondo-splash.jpg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
+          ),
+          Positioned.fill(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 100),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [Text("Cargando" , style: TextStyle(color: Colors.white, fontSize: 30),), CircularProgressIndicator()],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
