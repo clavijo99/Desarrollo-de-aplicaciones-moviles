d1c70dde33e08ffbf2585b60fed7402f9577d3a9
clavijo99 <bclavijogomez@gmail.com>
2025-09-19 10:00:00 -0500

    feat: Enhance login screen with user input field and updated styling

--- Flutter/navegacion/lib/pages/login.dart
+++ Flutter/navegacion/lib/pages/login.dart
@@ -11,6 +11,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController email = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +22,37 @@ class _LoginScreenState extends State<LoginScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text('Login'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: TextField(
+                decoration: InputDecoration(
+                  label: Text('Usuarios'),
+                  labelStyle: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  hintText: "example@gmail.com",
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 15,
+                  ),
+                  border: OutlineInputBorder(),
+                ),
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+                cursorColor: Colors.purple,
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  print(value);
+                },
+                controller: email,
+              ),
+            ),
             ButtonCustom(
               text: 'Registrar',
               onTap: () {
@@ -39,6 +72,16 @@ class _LoginScreenState extends State<LoginScreen> {
                 Navigator.of(
                   context,
                 ).pushNamed('/layout-screen', arguments: "Hola desde el login");
+              },
+            ),
+            ButtonCustom(
+              text: 'Imprimir input',
+              onTap: () {
+                //        Navigator.of(
+                //   context,
+                // ).push(MaterialPageRoute(builder: (_) => LayoutScreen()));
+
+                print(email.text);
               },
             ),
           ],
